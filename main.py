79b7ca0496258e77abd14894651fd0eff402d95c
MildaKastanauskaite <152031573+MildaKastanauskaite@users.noreply.github.com>
2026-01-22 22:55:22 +0200

    Add user name input and greeting functionality

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+def add_user_name ():
+    name = input ("Enter your name:")
+    return(name)
+
 def main ():
     print ("Python Fundamentals Project started")
-    
-main ()
+    user_name = add_user_name ()
+    print ("Hello", user_name)
+
+main()
