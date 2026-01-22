a2c4deea89155445c63312ac9532c27a2c26fca7
MildaKastanauskaite <152031573+MildaKastanauskaite@users.noreply.github.com>
2026-01-22 22:58:30 +0200

    Rename function add_user_name to get_user_name

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def add_user_name ():
-    name = input ("Enter your name:")
+def get_user_name ():
+    name = input ("Enter your name: ")
     return(name)
 
 def main ():
     print ("Python Fundamentals Project started")
-    user_name = add_user_name ()
+    user_name = get_user_name ()
     print ("Hello", user_name)
 
 main()
