634e3dc5bd8fcfd84eec5735f049bdf425394a75
MildaKastanauskaite <152031573+MildaKastanauskaite@users.noreply.github.com>
2026-01-23 16:15:50 +0200

    Add function to get user age and update main flow

--- main.py
+++ main.py
@@ -2,9 +2,27 @@ def get_user_name ():
     name = input ("Enter your name: ")
     return(name)
 
+def get_user_age ():
+    while True:
+        age = input("Enter your age: ")
+
+        if age.isdigit():
+            age = int(age)
+
+            if age > 0:
+                return age
+            else:
+                print("Age must be greater than 0.")
+        else:
+           print("Please enter a valid age.")     
+
 def main ():
     print ("Python Fundamentals Project started")
-    user_name = get_user_name ()
+    
+    user_name = get_user_name()
     print ("Hello", user_name)
 
+    user_age = get_user_age()
+    print ("Your age is", user_age)
+    
 main()
